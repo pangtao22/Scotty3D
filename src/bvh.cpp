52381ee1f470d8ee1fa0c7900fc846d2888dd72c
Unknown <pangtao@mit.edu>
2020-06-03 17:47:51 -0400

    BVH works!

--- src/bvh.cpp
+++ src/bvh.cpp
@@ -16,18 +16,17 @@ int computeBucket(int axis, int num_buckets, double coordinate,
   const double delta = bb.extent[axis] / num_buckets;
 
   int idx_bucket;
-  for(idx_bucket = 0; idx_bucket < num_buckets; idx_bucket++) {
+  for (idx_bucket = 0; idx_bucket < num_buckets; idx_bucket++) {
     double coordinate_division = delta * (idx_bucket + 1) + bb.min[axis];
-    if(coordinate_division > coordinate) {
+    if (coordinate_division > coordinate) {
       break;
     }
   }
   return idx_bucket;
 }
 
-template <class T>
-void printStlVector(const vector<T>& v) {
-  for(const auto& e : v) {
+template <class T> void printStlVector(const vector<T> &v) {
+  for (const auto &e : v) {
     cout << e << " ";
   }
   cout << endl;
@@ -38,78 +37,162 @@ struct Bucket {
   vector<size_t> primitives_indices;
 };
 
-void computePartitionBySah(const std::vector<Primitive*>& primitives,
-    const std::vector<Bucket>& buckets, const BBox& bb_parent,
-    size_t* min_idx, double* min_cost,
-    vector<size_t>* l_primitives, vector<size_t>* r_primitives) {
+void computePartitionBySah(const std::vector<Primitive *> &primitives,
+                           const std::vector<Bucket> &buckets,
+                           const BBox &bb_parent, size_t *min_idx,
+                           double *min_cost, vector<size_t> *l_primitives,
+                           vector<size_t> *r_primitives,
+                           BBox* left_bb_best, BBox* right_bb_best) {
   const double Sn = bb_parent.surface_area();
   *min_cost = INF_D;
   *min_idx = 0;
-  for(size_t i = 0; i < buckets.size() - 1; i++) {
-    BBox left;
-    BBox right;
+  for (size_t i = 0; i < buckets.size() - 1; i++) {
+    BBox left_bb;
+    BBox right_bb;
     size_t N_left = 0;
     size_t N_right = 0;
-    for(size_t j = 0; j <= i; j++) {
-      left.expand(buckets[j].bbox);
+    for (size_t j = 0; j <= i; j++) {
+      left_bb.expand(buckets[j].bbox);
       N_left += buckets[j].primitives_indices.size();
     }
-    for(size_t j = i+1; j < buckets.size(); j++) {
-      right.expand(buckets[j].bbox);
+    for (size_t j = i + 1; j < buckets.size(); j++) {
+      right_bb.expand(buckets[j].bbox);
       N_right += buckets[j].primitives_indices.size();
     }
-    double cost = 1 + left.surface_area() / Sn * N_left +
-        right.surface_area() / Sn * N_right;
-    if(cost < *min_cost) {
+    double cost = 1 + left_bb.surface_area() / Sn * N_left +
+                  right_bb.surface_area() / Sn * N_right;
+    if (cost < *min_cost) {
       *min_idx = i;
       *min_cost = cost;
+      *left_bb_best = left_bb;
+      *right_bb_best = right_bb;
     }
 
-//    // debug
-//    cout << "Partition " << i << ", cost: " << cost << endl;
-//    l_primitives->clear();
-//    for(size_t j = 0; j <= i; j++) {
-//      l_primitives->insert(
-//          l_primitives->end(),
-//          buckets[j].primitives_indices.begin(),
-//          buckets[j].primitives_indices.end());
-//    }
-//
-//    r_primitives->clear();
-//    for(size_t j = i + 1; j < buckets.size(); j++) {
-//      r_primitives->insert(
-//          r_primitives->end(),
-//          buckets[j].primitives_indices.begin(),
-//          buckets[j].primitives_indices.end());
-//    }
-//    cout << "Left:";
-//    printStlVector(*l_primitives);
-//    cout << left << endl;
-//    cout << "Right:" << endl;
-//    printStlVector(*r_primitives);
-//    cout << right << endl;
-//    cout << endl;
+    //    // debug
+    //    cout << "Partition " << i << ", cost: " << cost << endl;
+    //    l_primitives->clear();
+    //    for(size_t j = 0; j <= i; j++) {
+    //      l_primitives->insert(
+    //          l_primitives->end(),
+    //          buckets[j].primitives_indices.begin(),
+    //          buckets[j].primitives_indices.end());
+    //    }
+    //
+    //    r_primitives->clear();
+    //    for(size_t j = i + 1; j < buckets.size(); j++) {
+    //      r_primitives->insert(
+    //          r_primitives->end(),
+    //          buckets[j].primitives_indices.begin(),
+    //          buckets[j].primitives_indices.end());
+    //    }
+    //    cout << "Left:";
+    //    printStlVector(*l_primitives);
+    //    cout << left_bb << endl;
+    //    cout << "Right:" << endl;
+    //    printStlVector(*r_primitives);
+    //    cout << right << endl;
+    //    cout << endl;
   }
 
   l_primitives->clear();
-  for(size_t j = 0; j <= *min_idx; j++) {
-    l_primitives->insert(
-        l_primitives->end(),
-        buckets[j].primitives_indices.begin(),
-        buckets[j].primitives_indices.end());
+  for (size_t j = 0; j <= *min_idx; j++) {
+    l_primitives->insert(l_primitives->end(),
+                         buckets[j].primitives_indices.begin(),
+                         buckets[j].primitives_indices.end());
   }
 
   r_primitives->clear();
-  for(size_t j = *min_idx + 1; j < buckets.size(); j++) {
-    r_primitives->insert(
-        r_primitives->end(),
-        buckets[j].primitives_indices.begin(),
-        buckets[j].primitives_indices.end());
+  for (size_t j = *min_idx + 1; j < buckets.size(); j++) {
+    r_primitives->insert(r_primitives->end(),
+                         buckets[j].primitives_indices.begin(),
+                         buckets[j].primitives_indices.end());
   }
-
 }
 
+void divideBVHnode(std::vector<Primitive *>* primitives_ptr,
+    size_t max_leaf_size, const BBox& bb, BVHNode* node_p_ptr) {
+  if(node_p_ptr->range <= max_leaf_size) {
+    return;
+  }
 
+  // Find optimal left/right assignment along each axis.
+  BVHNode &node_p = *node_p_ptr;
+  auto &primitives = *primitives_ptr;
+
+//  cout << endl;
+//  int abc = 0;
+//  for(const auto& p : primitives) {
+//    cout << abc << " " << p->get_bbox() << endl;
+//    abc++;
+//  }
+
+  const int num_buckets = 8;
+  const int num_axes = 3;
+  vector<size_t> optimal_partition_idx(num_axes);
+  vector<double> sah_cost(num_axes);
+  vector<vector<size_t>> l_primitives_list(num_axes);
+  vector<vector<size_t>> r_primitives_list(num_axes);
+  vector<BBox> l_bbox_best(num_axes);
+  vector<BBox> r_bbox_best(num_axes);
+
+  for (int axis = 0; axis < num_axes; axis++) {
+    vector<Bucket> buckets(num_buckets);
+//    cout << "xyz: " << axis << endl;
+    for (int p = node_p.start; p < node_p.start + node_p.range; p++) {
+      int idx_bucket = computeBucket(axis, num_buckets,
+                                     primitives[p]->get_centroid()[axis], bb);
+      buckets[idx_bucket].primitives_indices.push_back(p);
+      buckets[idx_bucket].bbox.expand(primitives[p]->get_bbox());
+    }
+
+    computePartitionBySah(primitives, buckets, bb, &optimal_partition_idx[axis],
+                          &sah_cost[axis], &l_primitives_list[axis],
+                          &r_primitives_list[axis], &l_bbox_best[axis],
+                          &r_bbox_best[axis]);
+  }
+
+  // Get best primitives assignment.
+  const size_t best_axis = std::distance(
+      sah_cost.begin(), min_element(sah_cost.begin(), sah_cost.end()));
+  const auto &l_primitives = l_primitives_list[best_axis];
+  const auto &r_primitives = r_primitives_list[best_axis];
+  const auto &l_bbox = l_bbox_best[best_axis];
+  const auto &r_bbox = r_bbox_best[best_axis];
+
+//  cout << "Left:";
+//  printStlVector(l_primitives);
+//  cout << "Right:" << endl;
+//  printStlVector(r_primitives);
+//  cout << "Cost: " << sah_cost[best_axis] << endl;
+//  cout << endl;
+
+  // Rearrange primitives array.
+  vector<Primitive*> new_node_primitives(node_p.range);
+  int idx = 0;
+  for(const auto i : l_primitives) {
+    new_node_primitives[idx] = primitives[i];
+    idx++;
+  }
+  for(const auto i : r_primitives) {
+    new_node_primitives[idx] = primitives[i];
+    idx++;
+  }
+
+  for(size_t i = 0; i < node_p.range; i++) {
+    primitives[i + node_p.start] = new_node_primitives[i];
+  }
+
+  // Create new nodes
+  node_p.l = new BVHNode(l_bbox, node_p.start,
+                         l_primitives.size());
+  node_p.r = new BVHNode(r_bbox,
+                         node_p.start + l_primitives.size(),
+                         r_primitives.size());
+
+  // Continue to divide.
+  divideBVHnode(primitives_ptr, max_leaf_size, l_bbox, node_p.l);
+  divideBVHnode(primitives_ptr, max_leaf_size, r_bbox, node_p.r);
+}
 
 BVHAccel::BVHAccel(const std::vector<Primitive *> &_primitives,
                    size_t max_leaf_size) {
@@ -128,50 +211,8 @@ BVHAccel::BVHAccel(const std::vector<Primitive *> &_primitives,
 
   root = new BVHNode(bb, 0, primitives.size());
 
-  // x buckets.
-  const BVHNode &node = *root;
-  const int num_buckets = 8;
-  int num_axes = 2;
-  vector<size_t> optimal_partition_idx(num_axes);
-  vector<double> sah_cost(num_axes);
-  vector<vector<size_t> > l_primitives_list(num_axes);
-  vector<vector<size_t> > r_primitives_list(num_axes);
+  divideBVHnode(&primitives, max_leaf_size, bb, root);
 
-//  cout << endl;
-//  for(const auto& p : primitives) {
-//    cout << p->get_bbox() << endl;
-//  }
-
-  for(int axis = 0; axis < num_axes; axis++) {
-    vector<Bucket> buckets(num_buckets);
-    cout << "xyz: " << axis << endl;
-    for (int p = node.start; p < node.start + node.range; p++) {
-      int idx_bucket = computeBucket(
-          axis, num_buckets,primitives[p]->get_centroid()[axis], bb);
-      buckets[idx_bucket].primitives_indices.push_back(p);
-      buckets[idx_bucket].bbox.expand(primitives[p]->get_bbox());
-    }
-
-//    for(const auto& bucket : buckets) {
-//      cout << "Bucket:" << bucket.bbox << endl;
-//      printStlVector(bucket.primitives_indices);
-//      cout << endl;
-//    }
-
-    computePartitionBySah(primitives, buckets, bb,
-                          &optimal_partition_idx[axis], &sah_cost[axis],
-                          &l_primitives_list[axis],
-                          &r_primitives_list[axis]);
-    cout << "Left:";
-    printStlVector(l_primitives_list[axis]);
-    cout << "Right:" << endl;
-    printStlVector(r_primitives_list[axis]);
-    cout << "Cost: " << sah_cost[axis] << endl;
-    cout << endl;
-
-    
-
-  }
 }
 
 BVHAccel::~BVHAccel() {
